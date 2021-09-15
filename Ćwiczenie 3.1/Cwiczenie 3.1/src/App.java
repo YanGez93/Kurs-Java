8885280f7af96f76e06d345a8bc685fdd5944186
YanGez93 <yangez93@gmail.com>
2021-09-15 12:33:33 +0200

    azor

---
+++ Ćwiczenie 3.1/Cwiczenie 3.1/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Dog dog = new Dog();
+        System.out.println(dog.name + " to "+dog.race + " który ma "+dog.age+" lat.");
+    }
+}
