b21e99bfe8a66bdc0eac42d5ae99ff4a5fb1694c
YanGez93 <yangez93@gmail.com>
2021-09-15 11:19:01 +0200

    zgadywanka

--- Zgadywanka/Zgadywanka/src/App.java
+++ Zgadywanka/Zgadywanka/src/App.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int randomNumber = (int) (Math.random() * 101);  
+
+        int chances = 5;
+        int currentChance =0;
+        int inputRandomNumber;
+
+        System.out.println(randomNumber); 
+       
+        while(currentChance<chances)
+        {
+            inputRandomNumber = scanner.nextInt();
+
+            if(inputRandomNumber==randomNumber)
+            {
+                System.out.println("Brawo zgadłeś liczbę, a jej wartośc to "+ randomNumber); 
+                break;
+            }
+            else
+            {
+                System.out.println("Niestety to nie ta liczba, ta liczba jest "); 
+
+                if(inputRandomNumber>randomNumber)
+                {
+                    System.out.println(" mniejsza"); 
+                }
+                else
+                {
+                    System.out.println(" większa"); 
+                }
+            }
+        }
+        scanner.close();
     }
 }
