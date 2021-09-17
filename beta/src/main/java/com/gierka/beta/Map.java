91e5c0b7e3722ecad4e93155f0d80bb060f367dd
YanGez93 <yangez93@gmail.com>
2021-09-17 10:39:56 +0200

    aaa

--- beta/src/main/java/com/gierka/beta/Map.java
+++ beta/src/main/java/com/gierka/beta/Map.java
@@ -22,7 +22,6 @@ public class Map {
        
     }
 
-
     public void load()throws JsonParseException, JsonMappingException, IOException
     {
         String str = new String(Files.readAllBytes(jsonFile.toPath()));
@@ -33,7 +32,6 @@ public class Map {
         str = str.replaceAll( "\\[\\[","");
         str = str.replaceAll("\\]\\]","");
 
-        
         String[] strTable=str.split("],\\[");
 
         ArrayList<Grid> grids = new ArrayList<Grid>();   
@@ -42,9 +40,31 @@ public class Map {
         for(int i=0;i<strTable.length;i++)
         {
             Grid gridToAdd = new Grid();
+            String[] singleField = strTable[i].split(",");
+          //  System.out.println(strTable[i]);
+
+            
+            if(strTable[i].length()==8)
+            {
+                
+                gridToAdd.makeGrid(Integer.parseInt(singleField[0]),Integer.parseInt(singleField[1]),Integer.parseInt(singleField[2]),Integer.parseInt(singleField[3]));
+                grids.add(gridToAdd);
+                System.out.println(gridToAdd.texture+gridToAdd.x+gridToAdd.y+gridToAdd.z);
+                System.out.println(strTable[i]);
+            }
+            else if(strTable[i].length()==15)
+            {
+                
+                gridToAdd.makeGrid(Integer.parseInt(singleField[0]),Integer.parseInt(singleField[1]),Integer.parseInt(singleField[2]),Integer.parseInt(singleField[3]));
+                grids.add(gridToAdd);
+                System.out.println(gridToAdd.texture+gridToAdd.x+gridToAdd.y+gridToAdd.z);
+                System.out.println(strTable[i]);
+            }
+
+            /*
             if(strTable[i].length()==9)
             {
-                String[] strTable2= strTable[i].split(",");
+                
                 
                 gridToAdd.makeGrid(Integer.parseInt(strTable2[0]),Integer.parseInt(strTable2[1]),Integer.parseInt(strTable2[2]),Integer.parseInt(strTable2[3]));
                 grids.add(gridToAdd);
@@ -53,13 +73,14 @@ public class Map {
             else if(strTable[i].length()==7)
             {
 
-            }
-            
+            }   
+
+            */
 
         }
        
        // grids.add(e);
 
-        System.err.println(str);
+        //System.err.println(str);
     }
 }
